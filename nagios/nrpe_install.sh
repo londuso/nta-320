3b0fbab0744ccb9ed4e3494e11bac3fcda8c99aa
londuso <44388336+londuso@users.noreply.github.com>
2020-07-23 15:44:34 -0700

    Update nrpe_install.sh

--- nagios/nrpe_install.sh
+++ nagios/nrpe_install.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
+echo "[nti-310-epel]
+name=NTI310 EPEL
+baseurl=http://104.197.59.12/epel
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-base]
+name=NTI310 BASE
+baseurl=http://104.197.59.12/base
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-extras]
+name=NTI310 EXTRAS
+baseurl=http://104.197.59.12/extras/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-updates]
+name=NTI310 UPDATES
+baseurl=http://104.197.59.12/updates/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
 chmod 775 /etc/nagios/servers # added this from slack
 usermod -a -G root londuso35 (where nicolebade is your username) #added from slack.
 yum install -y nagios-plugins nrpe nagios-plugins-load nagios-plugins-ping nagios-plugins-disk nagios-plugins-httpd nagios-plugins-procs wget 
